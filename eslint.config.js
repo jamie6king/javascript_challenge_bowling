1e068655d61e142f34bd7c5855f78850a2fb74b3
Jamie King <jamiedanielking@gmail.com>
2024-11-13 13:46:32 +0000

    Expanded ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,17 @@
 const js = require("@eslint/js")
 const jest = require("eslint-plugin-jest")
 const globals = require("globals")
+const depend = require("eslint-plugin-depend")
 
 module.exports = [
     js.configs.recommended,
+    depend.configs["flat/recommended"],
     {
         plugins: {
             jest: jest
         },
         languageOptions: {
+            sourceType: "commonjs",
             globals: {
                 ...globals.node,
                 ...jest.environments.globals.globals
